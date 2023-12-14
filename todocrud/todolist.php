8f99856f3d7809f6638efc925180ae39a4e292ee
FathyaAzra <fathyaazra@gmail.com>
2023-12-14 18:12:20 +0700

    javascript tema done

--- todocrud/todolist.php
+++ todocrud/todolist.php
@@ -12,6 +12,7 @@ if(isset($username)) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>To-Do List Mantap</title>
+    <link rel="stylesheet" href="../style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   </head>
   <body>
@@ -19,6 +20,7 @@ if(isset($username)) {
     <nav class="navbar bg-body-tertiary">
         <div class="container-fluid">
         <a class="navbar-brand" href="../index.html">TO DO LIST</a>
+        <button id="themeToggle">Toggle Dark Mode</button>
         </div>
     </nav>
 
@@ -59,7 +61,7 @@ if(isset($username)) {
             </td>
         </tr>
     </table>
-
+    <script src = "../script.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   </body>
 </html>
